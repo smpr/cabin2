a1d7c3eb78a92197e227757c802169ef86e4f206
Brown <gxb8gqm@d25rh0s1gq17im.amer.homedepot.com>
2018-08-09 08:50:27 -0400

    navbar with working links

--- client/src/components/BoilerPlate/Nav.js
+++ client/src/components/BoilerPlate/Nav.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Nav extends Component {
   render() {
     return (
       <div>
-        Nav Bar
+       <RaisedButton href="/" label="Home" />
+       <RaisedButton href="/maintenance" label="Maintenance" />
+       <RaisedButton href="/reservation" label="Reservations" />
       </div>
     )
   }
