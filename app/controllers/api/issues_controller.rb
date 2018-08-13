7761dc95cacc00c1ea9252943eecace3bfa445e3
Brown <gxb8gqm@d25rh0s1gq17im.amer.homedepot.com>
2018-08-13 12:39:19 -0400

    issue create working

--- app/controllers/api/issues_controller.rb
+++ app/controllers/api/issues_controller.rb
@@ -7,6 +7,25 @@ class Api::IssuesController < ApplicationController
         @issue = Issue.find(params[:id])
         render json: @issue
     end
+    def create
+        # @user = current_user
+        # @category = @user.categories.new(category_params)
+        @issue = Issue.new(issue_params)
     
-
+        if @issue.save
+          render json: @issue, status: :created
+        else
+          render json: @issue.errors, status: :unprocessable_entity
+        end
+      end
+    # def create
+    #     @new_category = Category.new(category_params)
+    #     @new_category.save
+    #    puts "File saved"
+    #   end
+    private
+      
+    def issue_params
+        params.require(:issue).permit(:title, :ticketCreate, :quoteCost, :notes, :location, :issueCreate, :closed, :actualCost)
+    end
 end
--- client/src/components/Maintaince/MaintainceAdd.js
+++ client/src/components/Maintaince/MaintainceAdd.js
@@ -1,11 +1,101 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
+import { Container, FormContainer, BodyContainer, Style, TextLabelStyle } from "../StyledComponents/DefaultStyle"
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 export default class MaintainceAdd extends Component {
+  state = {
+    issue: [],
+    redirectToMaint: false
+  }
+  handleChange = (event) => {
+    const updateIssue = {
+        ...this.state.issue
+    }
+    updateIssue[event.target.name] = event.target.value
+    this.setState({ issue: updateIssue })
+}
+handleSubmit = async (event) => {
+  event.preventDefault()
+  await axios.post(`/api/issues`, { issue: this.state.issue })
+  this.setState({ redirectToMaint: true })
+
+}
   render() {
+    if (this.state.redirectToMaint) {
+      return <Redirect to="/maintenance" />
+    }
     return (
-      <div>
-        Page to add new maintaince items.
-      </div>
+      <BodyContainer>
+      <Container>
+          <form>
+              <FormContainer>
+                  <div>
+                      <div>
+                          <h2>Create an Issue:</h2>
+                      </div>
+                      <div>
+                          <TextField
+                              hintText="Issue Title"
+                              floatingLabelText="Issue Title"
+                              floatingLabelStyle={TextLabelStyle.floatingLabelStyle}
+                              floatingLabelFocusStyle={TextLabelStyle.floatingLabelFocusStyle}
+                              onChange={this.handleChange}
+                              name="title"
+                              type="text"
+                              required
+                              value={this.state.issue.title}
+                          />
+                      </div>
+                      <div>
+                          <TextField
+                              hintText="Location"
+                              floatingLabelText="Location"
+                              floatingLabelStyle={TextLabelStyle.floatingLabelStyle}
+                              floatingLabelFocusStyle={TextLabelStyle.floatingLabelFocusStyle}
+                              onChange={this.handleChange}
+                              name="location"
+                              type="text"
+                              required
+                              value={this.state.issue.location}
+                          />
+                      </div>
+                      <div>
+                          <TextField
+                              hintText="Quoted Cost"
+                              floatingLabelText="Quoted Cost"
+                              floatingLabelStyle={TextLabelStyle.floatingLabelStyle}
+                              floatingLabelFocusStyle={TextLabelStyle.floatingLabelFocusStyle}
+                              onChange={this.handleChange}
+                              name="quoteCost"
+                              type="text"
+                              required
+                              value={this.state.issue.quoteCost}
+                          />
+                      </div>
+                      <div>
+                          <TextField
+                              hintText="Notes"
+                              floatingLabelText="Notes"
+                              floatingLabelStyle={TextLabelStyle.floatingLabelStyle}
+                              floatingLabelFocusStyle={TextLabelStyle.floatingLabelFocusStyle}
+                              onChange={this.handleChange}
+                              name="notes"
+                              type="text"
+                              required
+                              value={this.state.issue.notes}
+                          />
+                      </div>
+                      <div>
+                          <RaisedButton onClick={this.handleSubmit} label="Create Issue" style={Style} />
+                      </div>
+                  </div>
+              </FormContainer>
+          </form>
+      </Container>
+  </BodyContainer>
     )
   }
 }
